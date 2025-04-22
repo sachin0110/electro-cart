526971c1d1c24112525da940eb4d267f62927846
Sachin Hatvalne <sachinhatvalne01@gmail.com>
2025-04-23 01:11:35 +0200

    fix: app loading issue fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
-import { CartComponent } from './features/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'cart', component: CartComponent },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/cart/cart.component').then((m) => m.CartComponent),
+  },
   { path: '**', redirectTo: '' },
 ];
